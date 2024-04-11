6961b21f260fe6556a5a6795da2fa9df06d57b63
Joao Vitor <joaovitormoraveis@gmail.com>
2024-04-10 21:19:00 -0400

    fix: component to hold configuration values

--- src/main/java/com/udemy/microservices/course/loan/dtos/LoanContactInfoDto.java
+++ src/main/java/com/udemy/microservices/course/loan/dtos/LoanContactInfoDto.java
@@ -5,12 +5,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
 
 @Data
-@Configuration
+@Component
 @ConfigurationProperties(prefix = "loans")
 public class LoanContactInfoDto {
 
